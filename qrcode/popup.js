f442c1105ab3ada16b54c1ef2191daedd094b4e1
huangwenming <huangwenming@baidu.com>
2018-01-31 20:06:48 +0800

    添加二维码复制功能

--- qrcode/popup.js
+++ qrcode/popup.js
@@ -59,4 +59,66 @@ var initQrcode = function(url) {
     jQuery('#qrcodeLbc').qrcode({
         text: pre.lbc
     });
+    var util = {
+        hasClass: function (obj, cls) {
+            return obj.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'));
+        },
+        addClass: function (obj, cls) {
+            if (!this.hasClass(obj, cls)) obj.className += " " + cls;
+        },
+        removeClass: function(obj, cls) {
+            if (this.hasClass(obj, cls)) {
+                var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
+                obj.className = obj.className.replace(reg, ' ');
+            }
+        }
+    }
+
+    document.querySelector('#app').addEventListener('click', function (e) {
+        var id = e.target.getAttribute('data-id');
+        console.log(id);
+        if (id) {
+            copyTextToClipboard(pre[id], function () {
+                showElement(e.target.previousElementSibling);
+            });
+        }
+    });
+    function showElement(el) {
+        util.removeClass(el, 'hidden');
+        setTimeout(function () {
+            util.addClass(el, 'hidden');
+        }, 2000);
+    }
+
+    function copyTextToClipboard(text, success) {
+        var textArea = document.createElement("textarea");
+
+        textArea.style.position = 'fixed';
+        textArea.style.top = 0;
+        textArea.style.left = 0;
+        textArea.style.width = '2em';
+        textArea.style.height = '2em';
+        textArea.style.padding = 0;
+        textArea.style.border = 'none';
+        textArea.style.outline = 'none';
+        textArea.style.boxShadow = 'none';
+        textArea.style.background = 'transparent';
+        textArea.value = text;
+
+        document.body.appendChild(textArea);
+
+        textArea.select();
+
+        try {
+            var msg = document.execCommand('copy') ? '成功' : '失败';
+            if (msg === '成功') {
+                success();
+            }
+            console.log('复制内容 ' + msg);
+        } catch (err) {
+            console.log('不能使用这种方法复制内容');
+        }
+
+        document.body.removeChild(textArea);
+    }
 }
